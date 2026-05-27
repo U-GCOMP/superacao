7746dc63c61aa6a5527c42fb6d9512e1467ee0f0
Rodrigo <bazingaf2004@outlook.com>
2026-05-27 16:22:34 -0300

    fix_dependencies_event_module

--- apps/backend/src/event/event.module.ts
+++ apps/backend/src/event/event.module.ts
@@ -5,13 +5,23 @@ import { EventController } from './event.controller';
 import { EventService } from './event.service';
 import { EventRepository } from './event.repository';
 import { EventRatingsRepository } from './eventRatings.repository';
+import { EventVolunteersRepository } from './eventVolunteers.repository';
 import { EventRatings } from './entities/event-rating.entity';
+import { EventVolunteers } from './entities/event-volunteers.entity';
 import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Event, EventRatings]), AuthModule],
+  imports: [
+    TypeOrmModule.forFeature([Event, EventRatings, EventVolunteers]),
+    AuthModule,
+  ],
   controllers: [EventController],
-  providers: [EventService, EventRepository, EventRatingsRepository],
+  providers: [
+    EventService,
+    EventRepository,
+    EventRatingsRepository,
+    EventVolunteersRepository,
+  ],
   exports: [EventService],
 })
 export class EventModule {}
